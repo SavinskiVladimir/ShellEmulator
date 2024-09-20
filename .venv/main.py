e7af9270bf37756af05b9264ca1ae319a0384d68
Владимир Савинский <vovka_savinskiy@mail.ru>
2024-09-20 22:04:31 +0300

    Доведение обработки команды cd до рабочего состояния, изменение обработки команд ls и tail для работы с файлами, находящимися только внутри текущей директории

--- .venv/main.py
+++ .venv/main.py
@@ -3,13 +3,6 @@ import os
 import logging
 import json
 
-def is_non_empty_tar_file(tarfile_path):
-  try:
-    with TarFile.open(tarfile_path, 'r') as tar:
-      return len(tar.getmembers()) > 0
-  except (TarError, FileNotFoundError):
-    return False
-
 # Настройка логгера
 logfile_path = 'log.json'  # путь к лог-файлу
 
@@ -48,12 +41,12 @@ logger = logging.getLogger('command_logger')
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
-
+# установка данных для вывода в привественном сообщении
 username = os.getlogin()  # получение имени пользователя
 hostname = os.environ['COMPUTERNAME']  # получение имени компьютера
 tarfile_path = 'files.tar'  # получение пути к архиву
 
-current_directory = 'D:/дз/конф_управление/shellemulator/.venv/files.tar'
+current_directory = '' # установка текущей директории (вначале - сама файловая система является директорией)
 
 initialize_log_file() # создание пустого лог-файла
 
@@ -66,17 +59,23 @@ with TarFile('files.tar', 'r') as files:
 
     if command == 'ls':
       for member in files.getmembers():
-         print(member.name)
+        if current_directory == '': # вывод содержимого самого верхнего каталога
+            if (member.name.find('/') == -1):
+                print(member.name)
+        elif member.name.startswith(current_directory + '/'): # вывод содержимого уставноленной директории
+            print(member.name[len(current_directory) + 1:])
     elif command == "exit":
       break
     elif command.startswith('cd'):
         path = command.split()[1]
-        if path == '..': # переход на уровень вверх
+        if path == '..':  # переход на уровень вверх
             if current_directory:
                 current_directory = '/'.join(current_directory.split('/')[:-1])
-        else: # Переход в указанный каталог
+                if not current_directory:  # если поднялись на уровень выше и текущий каталог пустой, устанавливаем его в корень
+                    current_directory = ''
+        else:  # переход в указанный каталог
             new_directory = current_directory + '/' + path if current_directory else path
-            # Проверка существования нового каталога в архиве
+            # проверка существования нового каталога в архиве
             if any(member.name.startswith(new_directory + '/') for member in files.getmembers()):
                 current_directory = new_directory
             else:
@@ -86,13 +85,14 @@ with TarFile('files.tar', 'r') as files:
     elif command.startswith('tail'):
         parts = command.split()
         filename = parts[1] if len(parts) > 1 else None
-        lines_to_show = int(parts[2]) if (len(parts) > 2) else 10 # Значение по умолчанию
+        lines_to_show = int(parts[2]) if (len(parts) > 2) else 10 # количество строк для вывода, по умолчанию - 10
         if filename:
             try:
-                content = files.extractfile(filename).read().decode()
-                lines = content.splitlines()
-                last_lines = lines[-lines_to_show:]  # Получаем последние строки
-                print("\n".join(last_lines))
+                full_path = f"{current_directory}/{filename}" if current_directory else filename
+                content = files.extractfile(full_path).read().decode() # получение содержимого файла
+                lines = content.splitlines() # разделение полученного содержимого на строки
+                last_lines = lines[-lines_to_show:]  # получение последние строки
+                print("\n".join(last_lines)) # сбор сообщения для вывода
             except KeyError:
                 print(f"Ошибка: файл '{filename}' не найден в архиве.")
             except Exception as e:
@@ -101,8 +101,8 @@ with TarFile('files.tar', 'r') as files:
             print("Ошибка: необходимо указать имя файла.")
     elif command == "history":
         try:
-            with open(logfile_path, 'r', encoding='UTF-8') as log:
-                data = json.load(log)
+            with open(logfile_path, 'r', encoding='UTF-8') as log: # открытие лог-файла для чтения
+                data = json.load(log) # чтение данных из лог-файла
                 for i in data:
                     print(i)
         except FileNotFoundError:
