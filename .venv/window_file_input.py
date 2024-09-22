83182da437cc4ebf114ae6c91d881d8d4a1ed1c8
Владимир Савинский <vovka_savinskiy@mail.ru>
2024-09-22 15:17:21 +0300

    Создание файла output.txt для дублирования результатов работы программы. Доработка window_file_input.py для работы с файлаими. Прикрепление последней версии лог-файла

--- .venv/window_file_input.py
+++ .venv/window_file_input.py
@@ -63,97 +63,107 @@ logger.addHandler(handler)
 
 current_directory = '' # задание текущей директории
 
-# Функция для обработки команд
-def execute_command(event):
-    global current_directory # добавление названия текущей директории в область видимости функции
-
-    command = command_entry.get() # получение команды
-    logger.info(command) # логгирование команды
-
-    # добавление введенной команды в текстовое поле
+# метод для обработки выходного сообщения
+def log_and_display_output(output):
     output_text.configure(state='normal')
-    output_text.insert(tk.END, f"{command}\n")
+    if output != '':
+        output_text.insert(tk.END, f"{output}\n") # вывод сообщения в GUI
+        # Запись вывода в файл
+        with open('output.txt', 'a', encoding='UTF-8') as out_file:
+            out_file.write(f"{output}\n") # вывод сообщения в консоль
     output_text.configure(state='disabled')
 
+# создание пустого выходного файла
+with open("output.txt", 'w'):
+    pass
+
+# функция для обработки команд
+def execute_command():
+    global current_directory # добавление названия текущей директории в область видимости функции
+    with open("commands.txt", 'r') as input:
+            commands = input.readlines()
     try:
         with TarFile(tarfile_path, 'r') as files:
-            if command == 'ls': # обработка команды ls
-                if current_directory == '': # вывод файлов из верхеней директории (файловой системы)
-                    output = "\n".join(member.name for member in files.getmembers() if member.name.find('/') == -1)
-                else: # вывод файлов из установленной директории
-                    output = "\n".join(member.name[len(current_directory) + 1:] for member in files.getmembers() if member.name.startswith(current_directory + '/'))
-            elif command == "whoami": # обработка команды whoami
-                output = username
-            elif command.startswith('cd'): # обработка команды cd
-                path = command.split()[1]
-                if path == '..':  # переход на уровень вверх
-                    if current_directory: # если текущая директория непустая
-                        current_directory = '/'.join(current_directory.split('/')[:-1])
-                        if not current_directory:
-                            current_directory = ''
-                        output = '' # выводимого сообщения нет
-                else:  # переход в нужную директорию
-                    new_directory = current_directory + '/' + path if current_directory else path
-                    # проверка существования новой директории
-                    if any(member.name.startswith(new_directory + '/') for member in files.getmembers()):
-                        current_directory = new_directory
-                        output = ''
+            for command in commands:
+                logger.info(command)
+                # добавление введенной команды в текстовое поле
+                output_text.configure(state='normal')
+                output_text.insert(tk.END, f"{command}")
+                output_text.configure(state='disabled')
+                if command[:-1] == 'ls': # обработка команды ls
+                    if current_directory == '': # вывод файлов из верхеней директории (файловой системы)
+                        output = "\n".join(member.name for member in files.getmembers() if member.name.find('/') == -1)
+                    else: # вывод файлов из установленной директории
+                        output = "\n".join(member.name[len(current_directory) + 1:] for member in files.getmembers() if member.name.startswith(current_directory + '/'))
+                elif command[:-1] == 'whoami': # обработка команды whoami
+                    output = username
+                elif command.startswith('cd'): # обработка команды cd
+                    path = command.split()[1]
+                    if path == '..':  # переход на уровень вверх
+                        if current_directory: # если текущая директория непустая
+                            current_directory = '/'.join(current_directory.split('/')[:-1])
+                            if not current_directory:
+                                current_directory = ''
+                            output = '' # выводимого сообщения нет
+                    else:  # переход в нужную директорию
+                        new_directory = current_directory + '/' + path if current_directory else path
+                        # проверка существования новой директории
+                        if any(member.name.startswith(new_directory + '/') for member in files.getmembers()):
+                            current_directory = new_directory
+                            output = ''
+                        else:
+                            output = f"Ошибка: каталог '{path}' не найден."
+                elif command.startswith('tail'): # обработка команды tail
+                    parts = command.split() # разделение введённой команды на части
+                    filename = parts[1] if len(parts) > 1 else None
+                    # задание количества строк для вывода, по умолчанию - 10
+                    lines_to_show = int(parts[2]) if (len(parts) > 2) else 10
+                    if filename:
+                        try:
+                            full_path = f"{current_directory}/{filename}" if current_directory else filename
+                            content = files.extractfile(full_path).read().decode()
+                            lines = content.splitlines()
+                            last_lines = lines[-lines_to_show:]  # получение последних строк
+                            output = "\n".join(last_lines)
+                        except KeyError:
+                            output = f"Ошибка: файл '{filename}' не найден в архиве."
+                        except Exception as e:
+                            output = f"Ошибка при чтении файла: {e}"
                     else:
-                        output = f"Ошибка: каталог '{path}' не найден."
-            elif command.startswith('tail'): # обработка команды tail
-                parts = command.split() # разделение введённой команды на части
-                filename = parts[1] if len(parts) > 1 else None
-                # задание количества строк для вывода, по умолчанию - 10
-                lines_to_show = int(parts[2]) if (len(parts) > 2) else 10
-                if filename:
+                        output = "Ошибка: необходимо указать имя файла."
+                elif command.startswith("exit"): # обработка команды exit
+                    window.quit()
+                    return
+                elif command[:-1] == "history": # обработка команды history
                     try:
-                        full_path = f"{current_directory}/{filename}" if current_directory else filename
-                        content = files.extractfile(full_path).read().decode()
-                        lines = content.splitlines()
-                        last_lines = lines[-lines_to_show:]  # получение последних строк
-                        output = "\n".join(last_lines)
-                    except KeyError:
-                        output = f"Ошибка: файл '{filename}' не найден в архиве."
+                        # открытие лог-файла для чтения
+                        with open(logfile_path, 'r', encoding='UTF-8') as log:
+                            data = json.load(log) # чтение данных из лог-файла
+                            output = ''
+                            for i in data:
+                                output += str(i) + "\n"
+                    except FileNotFoundError:
+                        output = f"Ошибка: Файл {logfile_path} не найден."
+                    except json.JSONDecodeError as e:
+                        output = f"Ошибка декодирования JSON: {e}. Проверьте корректность формата JSON в файле."
                     except Exception as e:
-                        output = f"Ошибка при чтении файла: {e}"
+                        output = f"Произошла ошибка: {e}."
                 else:
-                    output = "Ошибка: необходимо указать имя файла."
-            elif command == "exit": # обработка команды exit
-                window.quit()
-                return
-            elif command == "history": # обработка команды history
-                try:
-                    # открытие лог-файла для чтения
-                    with open(logfile_path, 'r', encoding='UTF-8') as log:
-                        data = json.load(log) # чтение данных из лог-файла
-                        output = ''
-                        for i in data:
-                            output += str(i) + "\n"
-                except FileNotFoundError:
-                    output = f"Ошибка: Файл {logfile_path} не найден."
-                except json.JSONDecodeError as e:
-                    output = f"Ошибка декодирования JSON: {e}. Проверьте корректность формата JSON в файле."
-                except Exception as e:
-                    output = f"Произошла ошибка: {e}."
-            else:
-                output = "Unknown command"
+                    output = "Unknown command"
 
-            # вывод результата в текстовое поле
-            output_text.configure(state='normal')
-            if output != '':
-                output_text.insert(tk.END, f"{output}\n")
-            output_text.configure(state='disabled')
+                # вывод результата в текстовое поле
+                log_and_display_output(output)
+                update_prompt()
     except Exception as e:
         output_text.configure(state='normal')
         output_text.insert(tk.END, f"Error: {str(e)}\n")
         output_text.configure(state='disabled')
 
     # очистка поля ввода
-    command_entry.delete(0, tk.END)
+    #command_entry.delete(0, tk.END)
 
     # обновление текста с приглашением ко вводу
     update_prompt()
-
 
 # функция для обновления приглашения к вводу
 def update_prompt():
@@ -179,13 +189,8 @@ output_text.pack(pady=10)
 # изначальное приглашение ко вводу
 update_prompt()
 
-# создание поля ввода для команд
-command_entry = tk.Entry(window, width=100)
-command_entry.pack(pady=10)
-command_entry.bind('<Return>', execute_command)  # Привязка клавиши Enter к функции
-
-# установка курсора в поле ввода при запуске
-command_entry.focus()
+# запуск цикла обработки команд
+execute_command()
 
 # создание пустого лой-файла
 initialize_log_file()
